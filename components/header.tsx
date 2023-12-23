9df96816c5ada5f5ccef2c8d5994c8f9db03c775
AO NEXUS LABS <121709547+SHOLAY1234@users.noreply.github.com>
2023-12-23 05:40:03 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -19,34 +19,31 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header className="flex flex-col sm:flex-row items-center justify-between px-4 md:px-8 bg-gradient-to-r from-blue-500 to-purple-500 text-white rounded-b-fullz mt-4 sm:h-24">
+    <header className="flex flex-col sm:flex-row items-center justify-between px-4 md:px-8 text-white rounded-30px mt-4 sm:h-24">
       {/* Logo */}
       <h1 className="m-0 mb-4 sm:mb-0">
         <Link href="https://amigos-odyssey.com/" target="_blank">
-        <img src="/logo.jpg" alt="Logo" className="h-14 sm:h-14 w-auto rounded-md" />
+          <img src="/logo.jpg" alt="Logo" className="h-14 sm:h-14 w-auto rounded-md" />
         </Link>
       </h1>
 
       {/* Buttons - Responsive Hamburger Menu */}
       <div className={`sm:hidden ${menuOpen ? 'flex flex-col' : 'hidden'}`}>
         <a
-          href="https://amigos-odyssey-click.vercel.app/"
-          target="_blank"
-          className="btn-twitter text-black font-bold hover:text-black hover:bg-blue-600 my-2"
+          href="https://twitter.com"
+          className="btn-twitter text- fontblack-bold hover:text-black hover:bg-green-600 my-2 rounded-30px"
         >
           AO Click
         </a>
         <a
-          href="https://discord.com/invite/xjVx6AekJs"
-          target="_blank"
-          className="btn-twitter text-black font-bold hover:text-black hover:bg-blue-600 my-2"
+          href="https://twitter.com"
+          className="btn-twitter text-black font-bold hover:text-black hover:bg-green-600 my-2 rounded-30px"
         >
           Discord
         </a>
         <a
-          href="https://twitter.com/amigosodyssey"
-          target="_blank"
-          className="btn-twitter text-black font-bold hover:text-black hover:bg-blue-600 my-2"
+          href="https://twitter.com"
+          className="btn-twitter text-black font-bold hover:text-black hover:bg-green-600 my-2 rounded-30px"
         >
           Twitter
         </a>
@@ -55,56 +52,47 @@ const Header: React.FC = () => {
       {/* Buttons - Desktop */}
       <div className="hidden sm:flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4">
         <a
-          href="https://twitter.com/amigosodyssey"
-          target="_blank"
-          className="btn-twitter text-black font-bold hover:text-black hover:bg-blue-600 transform transition-transform duration-300"
+          href="https://amigos-odyssey-click.vercel.app/"
+          className="btn-twitter text-black font-bold hover:text-black hover:bg-green-600 transform transition-transform duration-300 rounded-30px"
         >
-          Guide
+          AO CLICK
         </a>
         <a
-          href="https://amigos-odyssey-click.vercel.app/"
-          target="_blank"
-          className="btn-twitter text-black font-bold hover:text-black hover:bg-blue-600 transform transition-transform duration-300"
+          href="https://twitter.com/amigosodyssey"
+          className="btn-twitter text-black font-bold hover:text-black hover:bg-green-600 transform transition-transform duration-300 rounded-30px"
         >
-          AO Click
+          TWITTER
         </a>
         <a
           href="https://discord.com/invite/xjVx6AekJs"
-          target="_blank"
-          className="btn-twitter text-black font-bold hover:text-black hover:bg-blue-600 transform transition-transform duration-300"
+          className="btn-twitter text-black font-bold hover:text-black hover:bg-green-600 transform transition-transform duration-300 rounded-30px"
         >
-          Discord
-        </a>
-        <a
-          href="https://twitter.com/amigosodyssey"
-          target="_blank"
-          className="btn-twitter text-black font-bold hover:text-black hover:bg-blue-600 transform transition-transform duration-300"
-        >
-          Twitter
+          DISCORD
         </a>
       </div>
 
       {/* Responsive Hamburger Icon */}
       <button
-        className="sm:hidden block text-black font-bold focus:outline-none"
-        onClick={toggleMenu}
-      >
-        {menuOpen ? 'Close' : 'Menu'}
-      </button>
-{/* Wallet and Connect Button */}
-<div
-  className={`${
-    connected ? 'rounded-md bg-green-100 p-4' : 'rounded-md bg-purple-100 p-4'
-  }`}
+  className="sm:hidden block text-black font-bold focus:outline-none"
+  onClick={toggleMenu}
 >
-  {connected ? (
-    <Wallet />
-  ) : (
-    <Suspense fallback={<div>Loading...</div>}>
-      <ConnectButton />
-    </Suspense>
-  )}
-</div>
+  {menuOpen ? 'Close' : 'Menu'}
+</button>
+
+      {/* Wallet and Connect Button */}
+      <div
+        className={`${
+          connected ? 'rounded-md bg-green-100 p-4' : 'rounded-md bg-purple-100 p-4'
+        }`}
+      >
+        {connected ? (
+          <Wallet />
+        ) : (
+          <Suspense fallback={<div>Loading...</div>}>
+            <ConnectButton />
+          </Suspense>
+        )}
+      </div>
     </header>
   );
 };
