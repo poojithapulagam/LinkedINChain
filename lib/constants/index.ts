c3653dc59d841474a5b069ea2be5e57739eeff83
AO NEXUS LABS <121709547+SHOLAY1234@users.noreply.github.com>
2023-12-26 22:35:37 +0530

    Update index.ts

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -4,4 +4,4 @@ import type { Commitment } from '@solana/web3.js';
 export const COMMITMENT: Commitment = 'processed';
 
 export const PROGRAM_ID: Address =
-  '6kxXyeCqYKjkqXxjvQsPAGMECKKkYnUttRcb7FLYSX4R';
+  '6xJcQt5fPFRbh4hGiBCHHufop1bW8D5Mr1k1o5aF1VGR';
