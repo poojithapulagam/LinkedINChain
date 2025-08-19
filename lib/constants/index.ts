9c7218355a164905baca61acbe0fc8964d0f8a74
Poojitha Pulagam <103255409+poojithapulagam@users.noreply.github.com>
2025-08-19 23:37:50 +0530

    Update index.ts

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -4,4 +4,4 @@ import type { Commitment } from '@solana/web3.js';
 export const COMMITMENT: Commitment = 'processed';
 
 export const PROGRAM_ID: Address =
-  'HzWzBVgsm9AYiDfBtfkCfs4XYKWK2hGPGg7VmMZB7Vfx';
+  'DbcoQDkofHjxXSwivieFFYox8LFAnuuz3uaT5NNQZ5ga';
